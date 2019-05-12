fe0a54f5982e4d086a6af29e419adfb06a371b3d
Raymond Perminov <raymondproduction@ukr.net>
2019-05-12 22:27:55 +0300

    Added range input, change size of textarea

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 // Get the input field
 var input = document.getElementById("generator");
+var widthLine;
+var fontSize;
+var fontWeight;
+var fontFamily;
+
 document.addEventListener('DOMContentLoaded', (event) => {
+    widthLine = 400;
+    fontSize =12;
+    fontWeight = 'bold';
+    fontFamily = 'arial';
     document.getElementById("text").value = '';
-  })
+    document.getElementById('widthLine').value = widthLine;
+    setWidthLine(widthLine);
+    console.log('DOMContentLoaded');
+})
 
 function getTextWidth(text, font) {
     // re-use canvas object for better performance
@@ -13,6 +25,10 @@ function getTextWidth(text, font) {
     return metrics.width;
 }
 
+function setWidthLine(value) {
+    document.getElementById('valueWidthLine').innerText = value;
+}
+
 function refactor() {
  var text = document.getElementById("text").value;
  var textArray = text.replace(/\n/g, ' ').split(' ');
@@ -20,14 +36,26 @@ function refactor() {
  var j = 0;
  for (var i =0; i<textArray.length; i++) {
   while (
-         getTextWidth(textResult[i], "bold 12pt arial") < 400 &&
+         getTextWidth(
+             textResult[i],
+             fontWeight + " " + fontSize+ "pt "  + fontFamily
+             ) < widthLine &&
          textArray[j]
         ) {
     textResult[i] += textArray[j] + ' ';
-    console.log(getTextWidth(textResult[i], "bold 12pt arial"));
+    // console.log(getTextWidth(textResult[i], "bold 12pt arial"));
     j++;
   }
   textResult.push('');
  }
  document.getElementById('text_refactored').value = textResult.join('\n');
+ console.log('refactor');
 };
+
+function changeWidthLine() {
+    widthLine = document.getElementById('widthLine').value;
+    setWidthLine(widthLine);
+    refactor();
+    // console.log(document.getElementById('widthLine').value);
+    console.log('change width line');
+}
